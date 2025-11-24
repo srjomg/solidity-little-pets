1154f7cee6ec0dab285f27acbcc50fb5b9b0b2a7
srjomg <224263723+srjomg@users.noreply.github.com>
2025-11-24 23:06:34 +0400

    Init SimpleCounter contract

---
+++ src/basics/SimpleCouter/SimpleCounter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SimpleCounter {
+    uint256 public count;
+
+    constructor() {
+        count = 0;
+    }
+
+    function setCount(uint256 newCount) public {
+        count = newCount;
+    }
+
+    function increment() public {
+        count++;
+    }
+
+    function decrement() public {
+        count--;
+    }
+}
