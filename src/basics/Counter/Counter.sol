5c9304fac579c7e0bae9153f9c5ff172304a34d4
srjomg <224263723+srjomg@users.noreply.github.com>
2025-11-24 23:48:29 +0400

    Delete constructor. Add default values for state vars. Add cyclical mode for Counter

--- src/basics/Counter/Counter.sol
+++ src/basics/Counter/Counter.sol
@@ -2,21 +2,42 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public count;
-
-    constructor() {
-        count = 0;
-    }
+    uint256 public count = 0;
+    bool public cyclical = false;
 
     function setCount(uint256 newCount) public {
         count = newCount;
     }
 
+    function setCyclical(bool newCyclical) public {
+        cyclical = newCyclical;
+    }
+
     function increment() public {
+        uint256 max = type(uint256).max;
+
+        if (cyclical) {
+            if (count == max) {
+                count = 0;
+                return;
+            }
+        }
+
+        require(count < max, "Counter cannot be SO big");
         count++;
     }
 
     function decrement() public {
+        uint256 min = type(uint256).min;
+
+        if (cyclical) {
+            if (count == min) {
+                count = type(uint256).max;
+                return;
+            }
+        }
+
+        require(count > 0, "Counter cannot be negative");
         count--;
     }
 }
